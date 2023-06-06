58c3d225c552b0bd19b55c929a50a0f321fe5379
Robert Washbourne <rawashbourne@gmail.com>
2023-06-06 03:46:21 -0500

    testing...

--- src/sessionController.ts
+++ src/sessionController.ts
@@ -9,7 +9,8 @@ import 'subworkers'
 export const createSession = async (modelPath: string, proxy: boolean): Promise<Session | Comlink.Remote<Session>> => {
   if (proxy && typeof document !== "undefined") {
     ort.env.wasm.proxy = true;
-    const worker = new Worker("./session.worker.js", { type: "module" });
+    const url = new URL("./session.worker.js", import.meta.url);
+    const worker = new Worker(url.toString(), { type: "module" });
     const Channel = wrap<typeof Session>(worker);
     const session: Remote<Session> = await new Channel(SessionParams);
     await session.init(modelPath);
